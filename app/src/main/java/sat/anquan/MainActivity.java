62f4476815ef87ba0a71a7e202c8bed90db9dfa2
zzyyzz <nba20150609@163.com>
2016-12-27 10:15:32 +0800

    Initial commit

---
+++ app/src/main/java/sat/anquan/MainActivity.java
@@ -0,0 +1,44 @@
+package sat.anquan;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webview = (WebView) findViewById(R.id.webview);
+        //设置WebView属性，能够执行Javascript脚本
+        webview.getSettings().setJavaScriptEnabled(true);
+        //加载需要显示的网页
+        webview.loadUrl("file:///android_asset/te.html");
+        //设置Web视图
+        webview.setWebViewClient(new HelloWebViewClient ());
+    }
+
+    @Override
+    //设置回退
+    //覆盖Activity类的onKeyDown(int keyCoder,KeyEvent event)方法
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webview.canGoBack()) {
+            webview.goBack(); //goBack()表示返回WebView的上一页面
+            return true;
+        }
+        return false;
+    }
+
+    //Web视图
+    private class HelloWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+} 
